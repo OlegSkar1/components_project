4678a10cbfd0d60b01015b71ba0c4cc1d74ea943
Oleg Skarednov <olegskar1@gmail.com>
2022-10-25 15:28:34 +0500

    feat: form html+css

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,54 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  input = React.createRef<HTMLInputElement>();
+
   render() {
-    return <div className="mt-10 text-center">Form</div>;
+    return (
+      <>
+        <h1 className="mt-5 text-center text-4xl font-bold dark:text-white">
+          Карточка добавления продукта
+        </h1>
+        <form className="mt-7 w-1/3 m-auto">
+          <input
+            placeholder="Название продукта"
+            ref={this.input}
+            type="text"
+            id="title"
+            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <input
+            placeholder="Категория продукта"
+            type="text"
+            id="category"
+            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <input
+            placeholder="Стоимость"
+            type="number"
+            id="price"
+            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+          <textarea
+            id="message"
+            rows={4}
+            className="block mb-4 resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Введите описание продукта"
+          ></textarea>
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            htmlFor="file_input"
+          >
+            Загрузить картинку
+          </label>
+          <input
+            className="block mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+            accept="image/*"
+          />
+        </form>
+      </>
+    );
   }
 }
