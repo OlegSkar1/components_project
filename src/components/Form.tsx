a0a4cd576bdd81729fc221114c9a9dcdd29361f0
Oleg Skarednov <olegskar1@gmail.com>
2022-10-30 21:26:26 +0500

    feat form validate

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,8 +3,31 @@ import { Context, ContextInterface } from "../context";
 
 const fileReader = new FileReader();
 
+const buttonStyles = {
+  disabled:
+    'text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" disabled',
+  active:
+    "text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2",
+};
+
+const textareaInputStyles = {
+  normalInput:
+    "block mb-7 resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+  errorInput:
+    "block mb-7 resize-none p-2.5 w-full text-sm bg-gray-50 rounded-lg border dark:bg-gray-700  dark:placeholder-gray-400 dark:text-white  focus:border-pink-500 focus:ring-pink-500 border-pink-500 text-pink-600",
+};
+
+const fileInputStyles = {
+  normalFileInput:
+    "block mb-7 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400",
+  errorFileInput:
+    "block mb-7 w-full text-sm text-pink-600 bg-gray-50 rounded-lg border border-pink-500 cursor-pointer  focus:outline-none dark:bg-gray-700  dark:placeholder-gray-400 focus:border-pink-500 focus:ring-pink-500",
+};
+
 interface IFormState {
-  imageURL: string;
+  isActive: boolean;
+  isError: boolean;
+  isDescError: boolean;
 }
 export default class Form extends Component<unknown, IFormState> {
   static contextType = Context;
@@ -16,82 +39,218 @@ export default class Form extends Component<unknown, IFormState> {
   image = createRef<HTMLInputElement>();
   form = createRef<HTMLFormElement>();
 
-  handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
-    e.preventDefault();
+  state = {
+    isActive: false,
+    isError: false,
+    isDescError: false,
+  };
 
+  handleSubmit: React.FormEventHandler<HTMLFormElement> = (e): void => {
     const { addProduct } = this.context as ContextInterface;
 
-    const title = this.title.current?.value;
-    const image = this.image.current?.files?.item(0);
-    const description = this.description.current?.value;
-    const price = this.price.current?.value;
-    const category = this.category.current?.value;
+    const title = this.title.current;
+    const image = this.image.current;
+    const description = this.description.current;
+    const price = this.price.current;
+    const category = this.category.current;
     const id = Date.now();
+    if (!title?.value) {
+      e.preventDefault();
+      title!.required = true;
+    }
+    if (!category?.value) {
+      e.preventDefault();
+      category!.required = true;
+    }
+    if (!price?.value) {
+      e.preventDefault();
+      price!.required = true;
+    }
+    if (!description?.value) {
+      e.preventDefault();
+      this.setState({ isDescError: true });
+    }
+    if (!image?.files?.item(0)) {
+      e.preventDefault();
+      this.setState({ isError: true });
+    }
 
-    if (title && image && description && price && id && category) {
+    if (
+      title?.validity.valid &&
+      image?.files?.item(0) &&
+      description?.validity.valid &&
+      price?.validity.valid &&
+      category?.validity.valid
+    ) {
+      console.log("submit");
       fileReader.onloadend = () => {
         addProduct({
           id,
-          title,
-          category,
-          description,
+          title: title!.value,
+          category: category.value,
+          description: description.value,
           price: Number(price),
           image: fileReader.result as string,
         });
       };
-      fileReader.readAsDataURL(image);
+      fileReader.readAsDataURL(image?.files?.item(0) as File);
+    }
+  };
+
+  handleBlur: React.FormEventHandler<HTMLFormElement> = (e) => {
+    const event = e.target as HTMLFormElement;
+    switch (event.name) {
+      case event.name.match(/(productTitle)|(productCategory)/)
+        ? event.name
+        : true:
+        {
+          if (!event.value || event.value.length < 3) {
+            event.required = true;
+            event.pattern = "^[A-Za-zА-Яа-яЁё0-9]{3,16}$";
+          }
+        }
+        break;
+      case "productPrice":
+        {
+          if (!event.value) {
+            event.required = true;
+            event.pattern = "^[0-9]{1,8}$";
+          }
+        }
+        break;
+      case "productDescription":
+        {
+          if (event.value.length < 8) {
+            this.setState({ isDescError: true });
+          }
+        }
+        break;
+    }
+  };
+
+  handleChange: React.FormEventHandler<HTMLFormElement> = (e) => {
+    this.setState({ isActive: true });
+    const event = e.target as HTMLFormElement;
+    switch (event.name) {
+      case event.name.match(/(productTitle)|(productCategory)/)
+        ? event.name
+        : true:
+        {
+          if (event.value.length > 2) {
+            event.required = true;
+            event.pattern = "^[A-Za-zА-Яа-яЁё0-9]{3,16}$";
+          }
+        }
+        break;
+      case "productPrice":
+        {
+          if (event.value) {
+            event.required = true;
+            event.pattern = "^[0-9]{1,8}$";
+          }
+        }
+        break;
+      case "productDescription":
+        {
+          if (event.value.length >= 8) {
+            this.setState({ isDescError: false });
+          }
+          if (!event.value) {
+            this.setState({ isDescError: true });
+          }
+        }
+        break;
+      case "productImage":
+        {
+          if (event.files[0]) {
+            this.setState({ isError: false });
+          }
+        }
+        break;
     }
   };
 
   render() {
-    const { product } = this.context as ContextInterface;
-    console.log(product?.image);
+    const { isActive, isError, isDescError } = this.state;
+    const { active, disabled } = buttonStyles;
+    const { normalInput, errorInput } = textareaInputStyles;
+    const { normalFileInput, errorFileInput } = fileInputStyles;
+
     return (
       <>
         <h1 className="mt-5 text-center text-4xl font-bold dark:text-white">
           Карточка добавления продукта
         </h1>
         <form
+          noValidate
           id="form"
           ref={this.form}
           className="mt-7 w-1/3 m-auto"
-          onSubmit={this.handleSubmit}
+          onSubmit={isActive ? this.handleSubmit : (e) => e.preventDefault()}
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
         >
-          <input
-            placeholder="Название продукта"
-            type="text"
-            id="title"
-            name="productTitle"
-            ref={this.title}
-            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+          <div className="relative">
+            <input
+              placeholder="Название продукта"
+              type="text"
+              id="title"
+              name="productTitle"
+              ref={this.title}
+              className="peer mb-7 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:border-pink-500 invalid:text-pink-600"
+            />
+            <p className="text-sm text-red-600 dark:text-red-500 absolute top-[45px] left-[10px] invisible peer-invalid:visible ">
+              <span className="font-medium">Ошибка!</span> Введите 3-16
+              символов, кроме специальных
+            </p>
+          </div>
 
-          <input
-            placeholder="Категория продукта"
-            type="text"
-            id="category"
-            name="productCategory"
-            ref={this.category}
-            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+          <div className="relative">
+            <input
+              placeholder="Категория продукта"
+              type="text"
+              id="category"
+              name="productCategory"
+              ref={this.category}
+              className="mb-7 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:border-pink-500 invalid:text-pink-600 peer"
+            />
+            <p className="text-sm text-red-600 dark:text-red-500 absolute top-[45px] left-[10px] invisible peer-invalid:visible ">
+              <span className="font-medium">Ошибка!</span> Введите 3-16 символов
+            </p>
+          </div>
 
-          <input
-            placeholder="Стоимость"
-            type="number"
-            id="price"
-            ref={this.price}
-            name="productPrice"
-            className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+          <div className="relative">
+            <input
+              placeholder="Стоимость"
+              type="text"
+              id="price"
+              ref={this.price}
+              name="productPrice"
+              className="mb-7 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:border-pink-500 invalid:text-pink-600 peer"
+            />
+            <p className="text-sm text-red-600 dark:text-red-500 absolute top-[45px] left-[10px] invisible peer-invalid:visible ">
+              <span className="font-medium">Ошибка!</span> Введите 1-8 цифр
+            </p>
+          </div>
 
-          <textarea
-            id="message"
-            rows={4}
-            ref={this.description}
-            name="productDescription"
-            className="block mb-4 resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Введите описание продукта"
-          />
+          <div className="relative">
+            <textarea
+              id="message"
+              rows={8}
+              ref={this.description}
+              name="productDescription"
+              maxLength={800}
+              minLength={8}
+              className={isDescError ? errorInput : normalInput}
+              placeholder="Введите описание продукта"
+            />
+            {isDescError && (
+              <p className="text-sm text-red-600 dark:text-red-500 absolute top-[185px] left-[10px] ">
+                <span className="font-medium">Ошибка!</span> Введите 8-40
+                символов
+              </p>
+            )}
+          </div>
 
           <label
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -100,7 +259,7 @@ export default class Form extends Component<unknown, IFormState> {
             Загрузить картинку
           </label>
           <input
-            className="block mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            className={isError ? errorFileInput : normalFileInput}
             id="file_input"
             type="file"
             accept="image/*"
@@ -110,8 +269,9 @@ export default class Form extends Component<unknown, IFormState> {
 
           <input
             type="submit"
-            className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            className={isActive ? active : disabled}
             value="Добавить"
+            disabled={isActive ? false : true}
           />
         </form>
       </>
